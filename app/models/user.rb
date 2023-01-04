5d97b76c65816b00354d66168bf2a9cebe2197bb
timothymureithi <timothy.mureithi@student.moringaschool.com>
2023-01-04 23:52:14 +0300

    user model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :bookings 
+    has_many :properties
+    has_many :reviews, dependent: :destroy
+    has_many :bookings_as_owner, through: :properties, source: :bookings
+
 end
