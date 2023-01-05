810088191064ff763d0374da4b18197fb1109707
timothymureithi <timothy.mureithi@student.moringaschool.com>
2023-01-05 23:48:44 +0300

    user model: logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     has_many :reviews, dependent: :destroy
     has_many :bookings_as_owner, through: :properties, source: :bookings
 
+     # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
 end
