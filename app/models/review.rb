7b726322a667e84a8b906f9888ed3d0edf4fee1c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2023-01-07 22:14:53 +0300

    review model update

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+    validates :rating, inclusion: {in: (1..5)}
+    validates :user_id, :proerty_id, :body, :rating, presence: true
+  
+    belongs_to :property
+    belongs_to :user
 end
