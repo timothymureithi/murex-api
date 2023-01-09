9e3bd324e729b789ba380189b883005dffa071e9
timothymureithi <timothy.mureithi@student.moringaschool.com>
2023-01-09 09:23:42 +0300

    reservation model updated

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,15 @@
 class Reservation < ApplicationRecord
-    
+    validates :user_id, :listing_id, :check_in_date, :check_out_date, :num_guests, :price, presence: true
+  
+    belongs_to :guest,
+      foreign_key: :user_id,
+      class_name: :User
+  
+    belongs_to :listing,
+      foreign_key: :listing_id,
+      class_name: :Listing
+  
+    has_one :host,
+      through: :listing,
+      source: :host
 end
