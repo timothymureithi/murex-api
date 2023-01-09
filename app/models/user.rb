a3160d11705b63e4b4ae06c42a9419e2d5d848b4
timothymureithi <timothy.mureithi@student.moringaschool.com>
2023-01-09 09:20:47 +0300

    user's model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,58 @@
 class User < ApplicationRecord
-    has_many :bookings 
-    has_many :properties
-    has_many :reviews, dependent: :destroy
-    has_many :bookings_as_owner, through: :properties, source: :bookings
+  attr_reader :password
+  
+  validates :first_name, :last_name, :email, :password_digest, :session_token, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :email, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  
+  has_one_attached :profile_photo, dependent: :destroy
+  
+  has_many :listings,
+    foreign_key: :host_id,
+    class_name: :Listing
 
-     # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reservations, 
+    foreign_key: :user_id,
+    class_name: :Reservation,
+    dependent: :destroy
 
+  has_many :hosted_reservations,
+    through: :listings,
+    source: :reservations
+
+  has_many :reviews,
+    foreign_key: :reviewer_id,
+    class_name: :Review,
+    dependent: :destroy
+
+  # SPIRE
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+  
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+  
 end
