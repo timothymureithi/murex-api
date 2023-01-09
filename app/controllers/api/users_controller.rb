48e4049a25cbbf9d76491ebf7bb9cae7b616fc2d
timothymureithi <timothy.mureithi@student.moringaschool.com>
2023-01-09 09:34:06 +0300

    user controller :id/

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          login_user!(@user)
+          render "api/users/show"
+        else
+          render json: @user.errors.full_messages, status: 422
+        end
+      end
+    
+      def show
+        @user = User.includes(:reviews).find(params[:id])
+      end
+    
+      def update
+        @user = User.find_by(id: params[:id])
+        if @user.update(user_params)
+          render :show
+        else
+          render json: @user.errors.full_messages
+        end
+      end
+      
+      private
+    
+      def user_params
+        snake_case_params!(params[:user])
+        
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :profile_photo)
+      end
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,58 @@
 class Listing < ApplicationRecord
+    validates :host_id, :title, :description, :lat, :lng, :street, :city, :state, :country, :zip_code, :price, :additional_fees, :property_type, :num_guest, :num_beds, :num_baths, presence: true
+
+    before_validation :default, :on => :create
+    
+    belongs_to :host,
+      foreign_key: :host_id,
+      class_name: :User
+  
+    has_many :reservations,
+      foreign_key: :listing_id,
+      class_name: :Listing
+      
+    has_many :users,
+      through: :reservations,
+      source: :user
+      
+    has_many :reviews,
+      foreign_key: :listing_id,
+      class_name: :Review
+  
+    # Active Storage Associaiton (AWS S3)
+    has_many_attached :photos, dependent: :destroy
+  
+    has_one :host_photo,
+      through: :host,
+      source: :photo_attachment
+  
+    def default
+      self.price_currency ||= 'USD'
+      self.additional_fees ||= '0'
+    end
+    
+    def self.in_bounds(bounds)
+      self.where("lat < ?", bounds[:northEast][:lat])
+        .where("lat > ?", bounds[:southWest][:lat])
+        .where("lng > ?", bounds[:southWest][:lng])
+        .where("lng < ?", bounds[:northEast][:lng])
+    end
+  
+    def num_reviews
+      reviews.count
+    end
+    
+    def avg_rating
+  
+      return "New" if num_reviews == 0
+  
+      cleanliness = reviews.average(:cleanliness)
+      accuracy = reviews.average(:accuracy)
+      communication = reviews.average(:communication)
+      check_in = reviews.average(:check_in)
+      value = reviews.average(:value)
+      location = reviews.average(:location)
+  
+      return avg = ((cleanliness + accuracy + communication + check_in + value + location) / 6).round(2)
+    end
 end
