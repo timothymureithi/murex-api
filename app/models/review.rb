2a77d284a2eca3c1c2dc592ec897b44f7b390a86
timothymureithi <timothy.mureithi@student.moringaschool.com>
2023-01-09 09:22:38 +0300

    review model associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,13 +1,13 @@
 class Review < ApplicationRecord
     validates :reviewer_id, :listing_id, :message, presence: true
-  
-    validates :cleanliness, :accuracy, :communication, :check_in, :value, :location, presence: true, inclusion: { in: (1..5) }
-  
-    belongs_to :reviewer,
-      foreign_key: :reviewer_id,
-      class_name: :User
-  
-    belongs_to :listing,
-      foreign_key: :listing_id,
-      class_name: :Listing
+
+  validates :cleanliness, :accuracy, :communication, :check_in, :value, :location, presence: true, inclusion: { in: (1..5) }
+
+  belongs_to :reviewer,
+    foreign_key: :reviewer_id,
+    class_name: :User
+
+  belongs_to :listing,
+    foreign_key: :listing_id,
+    class_name: :Listing
   end
